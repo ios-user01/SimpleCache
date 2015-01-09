2a00b2e13f0004732074c1928323fbbc338fe93e
kishikawa katsumi <kishikawakatsumi@mac.com>
2015-01-10 05:29:12 +0900

    Add demo application

--- SimpleCache/ViewController.m
+++ SimpleCache/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import "SimpleCache.h"
 
 @interface ViewController ()
+
+@property (nonatomic) SimpleCache *imageCache;
+@property (nonatomic) NSArray *imageURLs;
 
 @end
 
@@ -16,10 +20,64 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.imageCache = [[SimpleCache alloc] init];
+    
+    NSMutableArray *imageURLs = [[NSMutableArray alloc] init];
+    for (int i = 1; i <= 15; i++) {
+        [imageURLs addObject:[NSString stringWithFormat:@"https://raw.githubusercontent.com/kishikawakatsumi/SimpleCache/image_api/SampleImages/%d.jpg", i]];
+    }
+    self.imageURLs = imageURLs;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
 }
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.imageURLs.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    ImageCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+    
+    NSURL *imageURL = [NSURL URLWithString:self.imageURLs[indexPath.row]];
+    cell.imageURL = imageURL;
+    
+    [self.imageCache cancelDownloadURL:imageURL];
+    
+    UIImage *image = [self.imageCache imageWithURL:imageURL placeholderImage:nil completionHandler:^(UIImage *image, NSURL *imageURL, NSError *error) {
+        if ([cell.imageURL isEqual:imageURL]) {
+            cell.photoView.image = [self resizedImageWithImage:image];
+        }
+    }];
+    cell.photoView.image = [self resizedImageWithImage:image];
+    
+    return cell;
+}
+
+- (UIImage *)resizedImageWithImage:(UIImage *)image {
+    if (!image) {
+        return nil;
+    }
+    
+    CGSize size = CGSizeApplyAffineTransform(image.size, CGAffineTransformMakeScale(0.05, 0.05));
+    UIGraphicsBeginImageContextWithOptions(size, NO, 0.0);
+    
+    [image drawInRect:(CGRect){CGPointZero, size}];
+    UIImage *resizedImage = UIGraphicsGetImageFromCurrentImageContext();
+    
+    UIGraphicsEndImageContext();
+    
+    return resizedImage;
+}
+
+@end
+
+@implementation ImageCell
 
 @end
